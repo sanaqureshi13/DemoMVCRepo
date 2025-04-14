c2a978cda2b0231d12b406d7e0d9bd653c50abc4
sanaqureshi13 <sanaaq1305@gmail.com>
2025-04-14 10:30:21 +0530

    First change to commit in new PR

--- DemoMvc/obj/Debug/net8.0/DemoMvc.AssemblyInfo.cs
+++ DemoMvc/obj/Debug/net8.0/DemoMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf319dd7d12038dd8669ffcb9d0428b2910c110")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
